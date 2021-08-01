07bcb30d0e1df3c0540baf38b8e344e41f604cfd
zakwan93 <zakwan_bhaiyat@yahoo.com>
2021-08-01 10:55:07 -0400

    Added validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ApplicationRecord
 
+  validates_presence_of :file
   # def self.import(file)
   #   CSV.foreach(file.path, headers: true,header_converters: :symbol) do |row|
   #     contact = find_by(fist_name: row["fist_name"]) || new
@@ -14,19 +15,17 @@ class Contact < ApplicationRecord
     header = spreadsheet.row(1)
     (2..spreadsheet.last_row).each do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
-      # contact = find_by_id(row["id"]) || new
       contact = find_by(first_name: row["first_name"]) || new
       contact.attributes = row.to_hash
-      # Contact.create! row.to_hash.delete_if { |k, _| k.blank? }
       contact.save! 
     end
   end
 
   def self.open_spreadsheet(file)
     case File.extname(file.original_filename)
-    # when ".csv" then Roo::CSV.new(file.path, file_warning: :ignore)
-    when ".xls" then Roo::Excel.new(file.path, file_warning: :ignore)
-    when ".xlsx" then Roo::Excelx.new(file.path, file_warning: :ignore)
+    # when ".csv" then Roo::CSV.new(file.path, packed: nil, file_warning: :ignore)
+    when ".xls" then Roo::Excel.new(file.path, packed: nil, file_warning: :ignore)
+    when ".xlsx" then Roo::Excelx.new(file.path, packed: nil, file_warning: :ignore)
     else raise "Unknown file type: #{file.original_filename}"
     end
   end
